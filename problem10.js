75a410a85226270bcf753a48c02199b860d77f7d
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-09 17:54:16 -0500

    anagrams

--- problem10.js
+++ problem10.js
@@ -1,36 +1,16 @@
 // Write a function that creates an anagram list, listing all the rearrangements of a given word. For example, if the user types "east", the program should list all 24 permutations, including "eats", "etas", "teas", and non-words like "tsae".
 
-function reverseString(str) {
-  //base case
-
-  if (str === '') {
-    return '';
+function anagrams(str, prefix = '') {
+  if (str.length === 1) {
+    console.log(prefix + str);
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      let currentChar = str.substring(i, i + 1);
+      let remaining = str.substring(i + 1);
+      let previous = str.substring(0, i);
+      anagrams(remaining + previous, prefix + currentChar);
+    }
   }
-
-  //recursive case
-  const firstChar = str[str.length - 1];
-
-  const restOfStr = str.slice(0, -1);
-
-  return firstChar + reverseString(restOfStr);
 }
 
-function anagrams(str) {
-  if (str.lenth === 1) {
-    return str;
-  } else if (str.length === 2) {
-    return [str, reverseString(str)];
-  }
-  let totalAnagrams = [];
-  for (let i = 0; i < str.length; i++) {
-    let subAnagram = [];
-    subAnagram = subAnagram.concat(
-      anagrams(str.substring(0, i) + str.substring(i + 1))
-    );
-    subAnagram = subAnagram.map((anagram) => str.charAt(i) + anagram);
-    totalAnagrams = subAnagram.concat(totalAnagrams);
-  }
-  return totalAnagrams;
-}
-
-console.log(anagrams('east'));
+anagrams('east');
